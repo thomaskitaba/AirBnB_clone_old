c5e04314afe9a1385fc23e33bd8434182bbfa20b
thomaskitaba <thomaskitaba@gmail.com>
2023-07-12 22:07:29 +0300

    update_file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,23 +12,25 @@ class FileStorage:
     def all(self):
         """returns the dictionary __objects"""
         return FileStorage.__objects
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = f"{obj.__class__.__name__}.{obj.id}"
 
-        return FileStorage.__objects[key] = obj #TODO:
+        return FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         temp_dict = {}
-            # >>> _objects = {'ob.11': obj1, 'ob.22': obj2,...}
-            for obj in FileStorage.__objects:
-                temp_dict[obj] = FileStorage.__objects[obj].to_dict()
-             # >>> temp_dict = {'ob.11': {'id': 11, 'name': aa,...}, {'id': 11, 'name': aa,...},...}
-            with open(FileStorage.__file_path, 'w') as db_f:
-                json_text = json.dumps(FileStorage.__objects)
-                db_f.write(json_text)
-                # or we can use json.dumps(FileStorage.__objects, db_f)
+        # >>> _objects = {'ob.11': obj1, 'ob.22': obj2,...}
+        for obj in FileStorage.__objects:
+            temp_dict[obj] = FileStorage.__objects[obj].to_dict()
+            # >>> temp_dict = {'ob.11': {'id': 11, 'name': aa,...},
+            # {'id': 11, 'name': aa,...},...}
+        with open(FileStorage.__file_path, 'w') as db_f:
+            json_text = json.dumps(FileStorage.__objects)
+            db_f.write(json_text)
+            # or we can use json.dumps(FileStorage.__objects, db_f)
 
     def reload(self):
         """deserializes the JSON file to __objects
@@ -36,13 +38,18 @@ class FileStorage:
         otherwise, do nothing. If the file doesn’t exist,
         no exception should be raised)"""
         temp_dict = {}
-        try
+        try:
             with open(FileStorage.__file_path, 'r') as db_f:
                 temp_dict = json.loads(db_f)
-                for obj in temp_dict.values():
-                    FileStorage.new(eval(obj["__class__"])(**obj))
-                    del obj["__class__"]
+                for dict_val in temp_dict.values():
+                    # convert dict_val to object of class __class__
+                    # since new(self, obj) method
+                    # accepts obj as parrameter
+                    FileStorage.new(eval(obj["__class__"])(**dict_val))
+                    # delete __class__ key since we dont need it
+                    # it will be generated every time we use to_dict()
+                    del dict_val["__class__"]
 
         except Exception as e:
-        # except FileNotFoundError:
+            # except FileNotFoundError:
             pass
