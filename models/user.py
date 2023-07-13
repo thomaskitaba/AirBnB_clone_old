c804d37d59d3265ad372221bc1cc72587d337405
thomaskitaba <thomaskitaba@gmail.com>
2023-07-13 23:09:38 +0300

    update user.py pycodestyle

--- models/user.py
+++ models/user.py
@@ -3,10 +3,9 @@
 from models.base_model import BaseModel
 
 
-
 class User(BaseModel):
     """ User class that inherits from BaseModel: """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    email = ""  # string - empty string
+    password = ""  # string - empty string
+    first_name = ""  # string - empty string
+    last_name = ""  # string - empty string
