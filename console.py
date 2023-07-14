97a5c38493c96a1f930f684d876975fe4a2cc6c0
thomaskitaba <thomaskitaba@gmail.com>
2023-07-14 22:54:23 +0300

    update do_update implementation of update no done

--- console.py
+++ console.py
@@ -231,8 +231,22 @@ class HBNBCommand(cmd.Cmd):
         elif len(new_arg) == 3:
             print("** value missing **")
         elif len(new_arg) == 4:
-            print("update {}".format(new_arg[0]))
-            storage.save()
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            if argl[2] in obj.__class__.__dict__.keys():
+                valtype = type(obj.__class__.__dict__[argl[2]])
+                obj.__dict__[argl[2]] = valtype(argl[3])
+            else:
+                obj.__dict__[argl[2]] = argl[3]
+        elif type(eval(argl[2])) == dict:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            for k, v in eval(argl[2]).items():
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
+                else:
+                    obj.__dict__[k] = v
+        storage.save()
 
 
 if __name__ == '__main__':
