996dfb592d30ada4635e127dc3e18245ca1ba8a1
thomaskitaba <thomaskitaba@gmail.com>
2023-07-16 14:08:35 +0300

    correcrtion_pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ class BaseModel:
     """ BaseModel class """
 
     def __init__(self, *args, **kwargs):
+        """ instansiate new base modesl object """
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
@@ -23,7 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+        models.storage.new(self)
 
     def __str__(self):
         """ __str__ """
