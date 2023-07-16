fc4cd1c569ab7ef89adc2cc85f5d0d1407327ec6
thomaskitaba <thomaskitaba@gmail.com>
2023-07-16 11:10:40 +0300

    correction_basemodel_kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,20 @@ class BaseModel:
     """ BaseModel class """
     def __init__(self, *args, **kwargs):
         """ instantiate new base model object """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__[k] = v
+                    self.__dict__[key] = kwargs[key]
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
