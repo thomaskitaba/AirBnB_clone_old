7b20cd97f1fb5f6ca296ef79224fb55435b566d3
thomaskitaba <thomaskitaba@gmail.com>
2023-07-16 14:16:06 +0300

    correction_check

--- models/base_model copy.py
+++ models/base_model copy.py
@@ -8,19 +8,16 @@ from datetime import datetime
 class BaseModel:
     """ BaseModel class """
     def __init__(self, *args, **kwargs):z
-        t_format = "%Y-%d-%mT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-        if kwargs:
-            for key, val in kwargs.items():
-                if kwargs[key] == "created_at":
-                    self.created_at = datetime.strptime(val, t_format)
-                elif kwargs[key] == "updated_at":
-                    self.updated_at = datetime.strptime(val, t_format)
-                elif kwargs[key] == "id":
-                    self.id = val
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
             models.storage.new(self)
 
