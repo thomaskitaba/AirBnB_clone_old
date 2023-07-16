ec1722c6e65bcef8c7c4f1fedd5a9c99a812fb0f
thomaskitaba <thomaskitaba@gmail.com>
2023-07-16 10:59:34 +0300

    update_.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,8 @@ from datetime import datetime
 class BaseModel:
     """ BaseModel class """
     def __init__(self, *args, **kwargs):
+        """ instantiate new base model object """
+
         t_format = "%Y-%d-%mT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
