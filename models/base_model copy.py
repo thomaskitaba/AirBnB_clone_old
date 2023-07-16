2794086d503f18dafbe4f0a25ad96e2c32e992f3
thomaskitaba <thomaskitaba@gmail.com>
2023-07-16 11:30:33 +0300

    update

--- models/base_model copy.py
+++ models/base_model copy.py
@@ -7,22 +7,21 @@ from datetime import datetime
 
 class BaseModel:
     """ BaseModel class """
-
-    def __init__(self, *args, **kwargs):
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+    def __init__(self, *args, **kwargs):z
+        t_format = "%Y-%d-%mT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        if kwargs:
+            for key, val in kwargs.items():
+                if kwargs[key] == "created_at":
+                    self.created_at = datetime.strptime(val, t_format)
+                elif kwargs[key] == "updated_at":
+                    self.updated_at = datetime.strptime(val, t_format)
+                elif kwargs[key] == "id":
+                    self.id = val
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
