286878051a441366d5af30eb2c36c82afcbc2d32
thomaskitaba <thomaskitaba@gmail.com>
2023-07-16 11:22:14 +0300

    correction_basemodel_kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -11,9 +11,7 @@ class BaseModel:
         """ instantiate new base model object """
 
         t_format = "%Y-%d-%mT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
         if kwargs is not None and kwargs != {}:
             for key, val in kwargs.items():
                 if key == "created_at":
@@ -23,6 +21,9 @@ class BaseModel:
                 elif
                     self.__dict__[key] = val
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
